0f0a5ffb8bac95586f1ae5f27a11dee3f90f182d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-01-19 16:18:13 +0100

    PLE: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: nokia Model: Nokia 6 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_PLE.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_PLE-user \
+    omni_PLE-userdebug \
+    omni_PLE-eng
